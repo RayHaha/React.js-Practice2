70f17e7dc5ff03673daa69b5b26c5cf5aa1c791a
RayHaha <g820324@gmail.com>
2018-11-15 22:13:02 -0800

    use the geolocation API to get a user physical location

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    {/* we use the getCurrentPosition function of the geolocation API to get the physical position
+    we have to pass in two separate function callbacks to get a response.
+    The first argument is going to be a function callback that gets called when successfully getting the user's location.
+    The second argument is going to be a failure callback */}
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
+
     return <div>Hi there!</div>;
 }
 
