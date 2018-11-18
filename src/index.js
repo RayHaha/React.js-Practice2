2b549933e385083f5be898170f3a6ec26a7d5870
RayHaha <g820324@gmail.com>
2018-11-17 16:50:43 -0800

    initialize State through constructor

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import ReactDOM from 'react-dom';
 // then we can't use the result of location in the return
 /*
 const App = () => {
-    we use the getCurrentPosition function of the geolocation API to get the physical position
-    we have to pass in two separate function callbacks to get a response.
-    The first argument is going to be a function callback that gets called when successfully getting the user's location.
-    The second argument is going to be a failure callback 
+    // we use the getCurrentPosition function of the geolocation API to get the physical position
+    // we have to pass in two separate function callbacks to get a response.
+    // The first argument is going to be a function callback that gets called when successfully getting the user's location.
+    // The second argument is going to be a failure callback 
     window.navigator.geolocation.getCurrentPosition(
         (position) => console.log(position),
         (err) => console.log(err)
@@ -27,6 +27,18 @@ const App = () => {
     We are subclassing React.Component
 */
 class App extends React.Component{
+    // constructor function is going to be called automatically and instantly before anything else
+    // so it's a very good location for us to initialize our state
+    constructor(props){
+        // it's required in constructor , you have to call super first
+        // to make sure that the parent(React.Component) constructor function get called
+        super(props);
+        // state must be initialized when a component is created
+        // we initialized our state object by creating a javascript object, lat for lattitude
+        this.state = { lat: null};
+    }
+
+    // React says we have to define render in every component!
     render(){
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
