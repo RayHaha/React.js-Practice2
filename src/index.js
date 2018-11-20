b70bcfe07ebac6cfeddd7c5961745a1e7bb270fc
RayHaha <g820324@gmail.com>
2018-11-19 17:57:57 -0800

    import SeasonDisplay and pass State as props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 // refactor the function component to class component
 // because that .getCurrentPosition cost a lot of time
@@ -75,7 +76,7 @@ class App extends React.Component{
         }
 
         if(!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }
 
         return <div>Loading!</div>;        
